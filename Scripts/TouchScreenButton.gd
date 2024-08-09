b804150510c0831f5832ad786f852bb2a27ab099
Atif Kekeç <kekecatf@outlook.com>
2024-08-09 13:20:14 +0300

    day2

--- Scripts/TouchScreenButton.gd
+++ Scripts/TouchScreenButton.gd
@@ -1,6 +1,6 @@
 extends TouchScreenButton
 
-
+@onready var timer = $Timer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,5 +9,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("Up"):
+		timer.start()
 		position.y -= 50
 	pass
+
+func _on_timer_timeout():
+	position.y += 50
+	print("timer durdu")
