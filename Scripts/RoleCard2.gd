e3c9cb842969d23457b8267a0aa42732d126b07d
Atif Kekeç <kekecatf@outlook.com>
2024-08-10 15:12:24 +0300

    day3

--- Scripts/RoleCard2.gd
+++ Scripts/RoleCard2.gd
@@ -1,6 +1,6 @@
 extends TouchScreenButton
 
-@onready var animationKart = $AnimationKart
+@onready var animationKart2 = $AnimationKart2
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,5 +9,5 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("Up"):
-		animationKart.play("Idle")
+		animationKart2.play("Idle")
 	pass
