39987a99bd6ea14be16b6476cee9ef544d74181c
Atif Kekeç <kekecatf@outlook.com>
2024-08-27 11:58:58 +0300

    day6

--- Scripts/Card.gd
+++ Scripts/Card.gd
@@ -1,21 +1,33 @@
 extends Node2D
-#Card Scripti
+# Card Scripti
 
 @onready var sprite = $Sprite2D
 @onready var timer = $TouchScreenButton/Timer
-@export var asset_name: String = "Zarf/Zarf.jpg"
+@export var asset_name: String
+var zarf = "Zarf/Zarf.jpg"
+var rol = "OyuncuRolleri/RollerArka.png"
+
+# Sabit boyutları belirleyin
+@export var target_width: int = 100
+@export var target_height: int = 150
 
 func _ready():
+	asset_name = zarf
 	var texture_path = "res://Assets/SecretHitlerAsset/" + asset_name
-	sprite.texture = load(texture_path)
+	var texture = load(texture_path)
+	
+	if texture:
+		sprite.texture = texture
+		_set_fixed_size(sprite)
 	pass 
 
-#func _process(delta):
-	#if Input.is_action_just_pressed("Up"):
-		#timer.start()
-		#position.y -= 50
-	#pass
-#
-#func _on_timer_timeout():
-	#position.y += 50
-	#print("timer durdu")
+func _set_fixed_size(sprite: Sprite2D):
+	# Texture'nin orijinal boyutunu al
+	var texture_size = sprite.texture.get_size()
+
+	# Ölçekleme oranlarını hesapla
+	var scale_x = target_width / texture_size.x
+	var scale_y = target_height / texture_size.y
+
+	# Sprite'in ölçeğini ayarla
+	sprite.scale = Vector2(scale_x, scale_y)
