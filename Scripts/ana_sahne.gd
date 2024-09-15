06f98a2a34780466c6c376993214fef135b33f51
Atif Kekeç <kekecatf@outlook.com>
2024-09-15 09:12:25 +0300

    day13

--- Scripts/ana_sahne.gd
+++ Scripts/ana_sahne.gd
@@ -4,7 +4,7 @@ extends Node2D
 @onready var GameModeNode = preload("res://Sceens/game_mode.tscn")  # GameMode sahnesini yükle
 var GameMode = null  # GameMode referansı
 var kart_konumlar = [Vector2(450, 550), Vector2(50, 250), Vector2(500, 50), Vector2(900, 300), Vector2(430,300)]  # Kartların konumları
-
+var kart_rotations = [0,90,-180,-90,0]
 
 func _ready():
 	GameMode = GameModeNode.instantiate()  # GameMode sahnesinden bir örnek oluştur
@@ -22,28 +22,34 @@ func kartlari_olustur():
 func kartlari_oyuncuya_goster(oyuncu_index, rol):
 	var offset_x = 50  # Kartların yan yana dizilmesi için x eksenindeki kaydırma miktarı
 	var base_position = kart_konumlar[oyuncu_index % len(kart_konumlar)]  # Oyuncu indexine göre konumu al
+	var kart_rotasyonu = kart_rotations[oyuncu_index % len(kart_konumlar)]
+	
 	# Rol kartını oluştur
 	var rol_karti = load("res://Sceens/card.tscn").instantiate()
 	rol_karti.asset_name = _rolKartiniAl(rol)  # Card scriptindeki asset_name değişkenine değer ata
 	rol_karti.position = base_position
+	rol_karti.rotation = kart_rotasyonu
 	add_child(rol_karti)
 
 	# Parti üyeliği kartını oluştur
 	var parti_karti = load("res://Sceens/card.tscn").instantiate()
 	parti_karti.asset_name = _partiKartiniAl(rol)  # Card scriptindeki asset_name değişkenine değer ata
 	parti_karti.position = base_position + Vector2(offset_x, 0)
+	parti_karti.rotation = kart_rotasyonu
 	add_child(parti_karti)
 
 	# Evet oylama kartını oluştur
 	var evet_karti = load("res://Sceens/card.tscn").instantiate()
 	evet_karti.asset_name = "Oylar/OylarEvet.png"  # Evet kartı için doğru asset'i ata
 	evet_karti.position = base_position + Vector2(offset_x * 2, 0)
+	evet_karti.rotation = kart_rotasyonu
 	add_child(evet_karti)
 
 	# Hayır oylama kartını oluştur
 	var hayir_karti = load("res://Sceens/card.tscn").instantiate()
 	hayir_karti.asset_name = "Oylar/OylarHayir.png"  # Hayır kartı için doğru asset'i ata
 	hayir_karti.position = base_position + Vector2(offset_x * 3, 0)
+	hayir_karti.rotation = kart_rotasyonu
 	add_child(hayir_karti)
 
 func _rolKartiniAl(rol):
