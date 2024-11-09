620b9b2ab1e34ecd543d1a35f409c7f2dbb6722a
Atif Kekeç <kekecatf@outlook.com>
2024-11-09 23:37:17 +0300

    day24

--- Scripts/politika_karti.gd
+++ Scripts/politika_karti.gd
@@ -1,16 +1,19 @@
 extends Area2D
 
+@onready var sprite = $Sprite2D
 var hedef_konum = Vector2(500, 300)  # Kartın gitmesini istediğiniz sabit hedef konum
 var hedefe_gitti = true  
 var is_original = true  # Orijinal kartı işaretlemek için değişken
+
 var politikalar = ["liberal politika","liberal politika","liberal politika",
 "liberal politika","liberal politika","liberal politika",
 "fasist politika","fasist politika","fasist politika","fasist politika",
 "fasist politika","fasist politika","fasist politika","fasist politika","fasist politika","fasist politika","fasist politika"]
+
 # Tıklama eventini gerçekleştiren fonksiyon
 func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		print("Kart tıklandı")
+		print("Politika Karti tıklandı")
 		
 		if hedefe_gitti and is_original:
 			# Sadece orijinal kart için Tween nesnesi oluşturuyoruz
@@ -31,6 +34,9 @@ func _on_tween_finished():
 		for i in range(2):
 			var new_kart = self.duplicate()  # Kartın kopyasını oluştur
 			new_kart.position = hedef_konum + Vector2((i + 1) * 150, 0)  # Yan yana dizmek için pozisyonu ayarla
+			var texture_path = "res://SecretHitlerAsset/Politikalar/PolitikalarFasist.png"
+			var texture = load(texture_path)
+			sprite.texture = texture  # Texture'yi TouchScreenButton'a ata
 			
 			# Yeni kartların `is_original` değeri `false` olarak ayarlanır
 			new_kart.is_original = false  
