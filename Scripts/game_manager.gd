e68915d83fa76a509bfcff680a5864549ad188e6
Atif Kekeç <kekecatf@outlook.com>
2024-11-11 13:29:50 +0300

    day25

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -1,3 +1,4 @@
 extends Node
 
 var game_mode = null
+var atilacak_kart_secildimi = false
--- Scripts/politika_karti.gd
+++ Scripts/politika_karti.gd
@@ -4,10 +4,10 @@ extends Area2D
 var hedef_konum = Vector2(500, 300)  # Kartın gitmesini istediğiniz sabit hedef konum
 var is_original = true  # Orijinal kartı işaretlemek için değişken
 
-var politikalar = ["liberal politika","liberal politika","liberal politika",
-"liberal politika","liberal politika","liberal politika",
-"fasist politika","fasist politika","fasist politika","fasist politika",
-"fasist politika","fasist politika","fasist politika","fasist politika","fasist politika","fasist politika","fasist politika"]
+var politikalar = ["liberal politika", "liberal politika", "liberal politika",
+"liberal politika", "liberal politika", "liberal politika",
+"fasist politika", "fasist politika", "fasist politika", "fasist politika",
+"fasist politika", "fasist politika", "fasist politika", "fasist politika", "fasist politika", "fasist politika", "fasist politika"]
 
 var asset_paths = {
 	"liberal politika": "res://SecretHitlerAsset/Politikalar/PolitikalarLiberal.png",
@@ -19,10 +19,12 @@ func _ready():
 
 # Tıklama eventini gerçekleştiren fonksiyon
 func _input_event(viewport, event, shape_idx):
+	# Kart seçildiyse başka kartların etkileşim almasını engelle
+
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		print("politika karti tiklandi")
 		
-		if is_original:
+		if position == Vector2(250, 230):
 			# Sadece orijinal kart için Tween nesnesi oluşturuyoruz
 			var tween = create_tween()
 			tween.connect("finished", Callable(self, "_on_tween_finished"))
@@ -30,8 +32,8 @@ func _input_event(viewport, event, shape_idx):
 			# Hedef konuma hareket ve ölçek ayarı
 			tween.tween_property(self, "position", hedef_konum, 1)
 			tween.tween_property(self, "scale", Vector2(0.2, 0.2), 1)
-			# Hedefe gitme durumunu tersine çeviriyoruz
-		else:
+			
+		elif GameManager.atilacak_kart_secildimi == false:
 			# Tıklanan kartın görselini güncellemek için yeni bir görsel yol belirleyin
 			var yeni_gorsel = "res://SecretHitlerAsset/Politikalar/PolitikaArka.png"  # Yeni sprite yolu
 			sprite.texture = load(yeni_gorsel)
@@ -39,23 +41,26 @@ func _input_event(viewport, event, shape_idx):
 			# Kartın yeni konuma gitmesini sağla
 			var yeni_konum = Vector2(846, 230)  # Belirlediğiniz yeni konum
 			var tween = create_tween()
+			print(GameManager.atilacak_kart_secildimi)
+			GameManager.atilacak_kart_secildimi = true  # Kart seçildi olarak işaretleniyor
+			print(GameManager.atilacak_kart_secildimi)
 			tween.tween_property(self, "position", yeni_konum, 1)
 			tween.tween_property(self, "scale", Vector2(0.08, 0.08), 1)
 
 # Tween tamamlandığında çağrılacak fonksiyon
 func _on_tween_finished():
 	queue_free()
-	if is_original:
-			# Üstteki 3 kartı çek ve göster
-			for i in range(3):
-				var kart_turu = politikalar.pop_front()  # En üstteki kartı çek
-				var kart_gorsel_yolu = asset_paths[kart_turu]  # Kartın görsel yolunu al
-				# Yeni bir kart nesnesi oluştur
-				var new_kart = self.duplicate()  
-				new_kart.is_original = false  # Kopya kart olarak işaretle
-				new_kart.position = hedef_konum + Vector2((i) * 150, 0)  # Konumu ayarla
 
-				# Sprite'ın görselini güncelle
-				new_kart.get_node("Sprite2D").texture = load(kart_gorsel_yolu)
-				
-				get_parent().add_child(new_kart)  # Kartı sahneye ekle
+	# Üstteki 3 kartı çek ve göster
+	for i in range(3):
+		var kart_turu = politikalar.pop_front()  # En üstteki kartı çek
+		var kart_gorsel_yolu = asset_paths[kart_turu]  # Kartın görsel yolunu al
+		# Yeni bir kart nesnesi oluştur
+		var new_kart = self.duplicate()
+		new_kart.is_original = false  # Kopyaların orijinal olmadığını belirtiyoruz
+		new_kart.position = hedef_konum + Vector2((i) * 150, 0)  # Konumu ayarla
+
+		# Sprite'ın görselini güncelle
+		new_kart.get_node("Sprite2D").texture = load(kart_gorsel_yolu)
+		
+		get_parent().add_child(new_kart)  # Kartı sahneye ekle
