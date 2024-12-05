7e89576e75446431df02034d9bf26959a97b2c25
Atif Kekeç <kekecatf@outlook.com>
2024-12-05 12:46:49 +0300

    day29

--- Scripts/sansolye_secme.gd
+++ Scripts/sansolye_secme.gd
@@ -1,27 +1,29 @@
 extends Area2D
 
-var is_dragging = false
+var is_dragging = false  # Sürüklenme durumunu takip eder
+var offset = Vector2()   # Farenin tıklama pozisyonuna göre nesneye olan mesafe
+var hedef_alan = Rect2(Vector2(830, 320), Vector2(960 - 830, 450 - 320))  # Hedef alan (dikdörtgen)
 
-#Tıklama eventini gerçekleştiren fonksiyon
 func _input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.pressed:
+	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:
-			print("sansolye sec tiklandi")
+			if event.pressed:
+				# Sol tıklama başladı, sürükleme aktif
+				is_dragging = true
+				offset = global_position - get_global_mouse_position()
+			else:
+				# Sol tıklama bitti, sürükleme durdur ve hedef kontrolü yap
+				is_dragging = false
+				_hedef_alan_kontrol()
 
-func _input(event):
-	# Sürüklemeye başlama
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		if get_global_mouse_position().distance_to(global_position) < 50: # Fare tıklamasının cisme yakınlığı
-			is_dragging = true
+func _process(delta):
+	if is_dragging:
+		# Fareyi takip ederek nesneyi hareket ettir
+		global_position = get_global_mouse_position() + offset
 
-	# Sürükleme sırasında nesneyi fare pozisyonuna taşıma
-	elif event is InputEventMouseMotion and is_dragging:
-		global_position = get_global_mouse_position()
-
-	# Fare bırakıldığında sürüklemeyi durdurma
-	elif event is InputEventMouseButton and not event.pressed:
-		if is_dragging:
-			is_dragging = false
-			# Hedef alanın konumunu kontrol etme
-			if get_parent().get_node("TargetArea").global_position.distance_to(global_position) < 50:
-				print("Cisim hedef alana bırakıldı!")  # İstediğiniz çıktıyı burada alabilirsiniz
+# Hedef alanı kontrol eden fonksiyon
+func _hedef_alan_kontrol():
+	if hedef_alan.has_point(global_position):
+		print("Hedef alanın içine bırakıldı!")
+	else:
+		print("Hedef alanın dışında bırakıldı.")
