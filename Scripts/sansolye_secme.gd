1de0e91b07ca7ef965ca4fe8654d85d32e509b6b
Atif Kekeç <kekecatf@outlook.com>
2024-12-05 16:41:37 +0300

    day29

--- Scripts/sansolye_secme.gd
+++ Scripts/sansolye_secme.gd
@@ -2,7 +2,19 @@ extends Area2D
 
 var is_dragging = false  # Sürüklenme durumunu takip eder
 var offset = Vector2()   # Farenin tıklama pozisyonuna göre nesneye olan mesafe
-var hedef_alan = Rect2(Vector2(830, 320), Vector2(960 - 830, 450 - 320))  # Hedef alan (dikdörtgen)
+var oyuncu_alanlari = [
+	Rect2(Vector2(50, 3), Vector2(130, 130)),  # Oyuncu 1'in alanı
+	Rect2(Vector2(600, 320), Vector2(130, 130)),  # Oyuncu 2'nin alanı
+	Rect2(Vector2(400, 200), Vector2(130, 130)),  # Oyuncu 3'ün alanı
+	Rect2(Vector2(200, 400), Vector2(130, 130)),  # Oyuncu 4'ün alanı
+	Rect2(Vector2(100, 600), Vector2(130, 130)),  # Oyuncu 5'in alanı
+	Rect2(Vector2(830, 600), Vector2(130, 130)),  # Oyuncu 6'nın alanı
+	Rect2(Vector2(1050, 320), Vector2(130, 130)), # Oyuncu 7'nin alanı
+	Rect2(Vector2(1250, 400), Vector2(130, 130)), # Oyuncu 8'in alanı
+	Rect2(Vector2(1400, 500), Vector2(130, 130)), # Oyuncu 9'un alanı
+	Rect2(Vector2(1600, 700), Vector2(130, 130))  # Oyuncu 10'un alanı
+]
+
 
 func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
@@ -23,7 +35,8 @@ func _process(delta):
 
 # Hedef alanı kontrol eden fonksiyon
 func _hedef_alan_kontrol():
-	if hedef_alan.has_point(global_position):
-		print("Hedef alanın içine bırakıldı!")
-	else:
-		print("Hedef alanın dışında bırakıldı.")
+	for i in range(oyuncu_alanlari.size()):
+		if oyuncu_alanlari[i].has_point(global_position):
+			print("Nesne Oyuncu %d'nin alanında!" % (i + 1))
+			return
+	print("Nesne hiçbir oyuncu alanına bırakılmadı!")
